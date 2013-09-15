0775b451a3bf9a8654dbd85da77c4b93e80e555c
PapaMarky <papamarky@markyshouse.com>
2013-09-15 08:58:27 -0700

    Have stable touch switch HW& SW

--- CampingNightLight/CampingNightLight.ino
+++ CampingNightLight/CampingNightLight.ino
@@ -4,10 +4,7 @@ const int PWM_1 = 3; // the transistor
 const int LOW_BATTERY_LED = 4;
 
 const int PHOTOCELL = A0;
-const int DIMMER = A1;
 const int BATTERY = A2;
-const int SW_IN = 8;  // cols
-const int SW_OUT = 12; // rows
 
 const int TOUCH_SENSOR = 10;
 
@@ -21,11 +18,7 @@ void setup() {
   pinMode(PWM_1, OUTPUT);
   pinMode(LOW_BATTERY_LED, OUTPUT);
   pinMode(BATTERY, INPUT);
-  pinMode(SW_IN, INPUT);
-  pinMode(SW_OUT, OUTPUT);
   pinMode(PHOTOCELL, INPUT);
-  //pinMode(DIMMER, INPUT);
-  digitalWrite(SW_OUT, HIGH);
 }
 static float brightness = 255;
 const int off_threshhold = 190;
@@ -33,39 +26,6 @@ const int on_threshhold = 130;
 static bool battery_is_low = false;
 
 static bool switch_on = false;
-const int DELAY = 50; // 25
-bool check_switch() {
-  Serial.print("check_switch, on: ");
-  Serial.println(switch_on ? "true" : "false");
-  digitalWrite(SW_OUT, HIGH);
-  delay(DELAY);
-  if (!switch_on) {
-    // better switch bounce checking?
-    if (digitalRead(SW_IN) == LOW) {
-      Serial.println(" -- first LOW");
-      delay(DELAY);
-    }
-    if (digitalRead(SW_IN) == LOW) {
-      Serial.println(" -- second LOW");
-      switch_on = true; 
-    }
-  }
-  else {
-    if (digitalRead(SW_IN) == HIGH) {
-      Serial.println(" -- first HIGH");
-      delay(DELAY);
-      if (digitalRead(SW_IN) == HIGH) {
-        Serial.println(" -- second HIGH");
-        switch_on = false;
-      }
-    }
-    digitalWrite(SW_OUT, LOW);
-    delay(DELAY*2);
-  }
-  
-  return switch_on;
-}
-
 void loop() {
   static bool on = false;
   int light = analogRead(PHOTOCELL);
@@ -76,7 +36,6 @@ void loop() {
   float battery = 5.0 * (float)analogRead(BATTERY)/1024.0;
   uint8_t cap = readCapacitivePin(TOUCH_SENSOR);
   bool swtch = cap > 5; //check_switch();
-//  bool swtch = check_switch();
   Serial.print("CAP: ");
   Serial.println(cap);
   //Serial.print("battery level: ");
@@ -90,16 +49,6 @@ void loop() {
     battery_is_low = false;
     digitalWrite(LOW_BATTERY_LED, LOW);
   }
-  
-  //Serial.print("battery is low: ");
-  //Serial.println(battery_is_low ? "true" : "false");
-  /*
-  Serial.print("dimmer: ");
-  Serial.print(d);
-  Serial.print(" : ");
-  Serial.print(dimmer, 2);
-  Serial.println("");
-  */
   on = (light < (on ? off_threshhold : on_threshhold));
   int brightness = (int)max(255.0 * dimmer, 1.0);
   int b = on ? brightness : 0;
@@ -109,13 +58,11 @@ void loop() {
     last_b = b;
     analogWrite(PWM_1, b);
   }
-  delay(DELAY*2);
-  /*
+
   if (on)
     delay(10);
   else
     delay(1000); // Narcoleptic.delay(1000);
-    */
   
 }
 
