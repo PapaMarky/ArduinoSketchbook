3e40a8a14359af5f9c2f4c6b42b53b91398edb5f
PapaMarky <papamarky@markyshouse.com>
2014-06-28 10:22:46 -0700

    DAC player

--- libraries/Music/Music.h
+++ libraries/Music/Music.h
@@ -3,10 +3,10 @@
 #include <Arduino.h>
 
 namespace mdlib {
-
+  bool LED = false;
   // Structure for holding note / duration pairs
   struct music_note {
-    int frequency;     // integer frequency
+    long frequency;     // integer frequency
     int beats;
   };
   
@@ -16,8 +16,8 @@ namespace mdlib {
     ~Tune(){};
 
     // Define the Tune by setting beat duration and note data.
-    void SetBeatDuration(uint8_t duration) { beat_duration_ = duration; }
-    uint8_t BeatDuration() { return beat_duration_; }
+    void SetBeatDuration(int duration) { beat_duration_ = duration; }
+    int BeatDuration() { return beat_duration_; }
 
     void SetNotes(music_note* notes, int length) {
       notes_ = notes;
@@ -26,6 +26,14 @@ namespace mdlib {
 
     bool Start() {
       current_note_ = 0;
+      if (current_note_ < length_) {
+	current_frequency_ = (notes_ + current_note_)->frequency;
+	current_duration_ = (notes_ + current_note_)->beats * beat_duration_;
+      }
+      else {
+	current_frequency_ = 0;
+	current_duration_ = 0;
+      }
       return current_note_ < length_;
     }
 
@@ -33,9 +41,24 @@ namespace mdlib {
     bool NextNote() {
       if (current_note_ < length_)
 	current_note_++;
+
+      LED = !LED;
+      digitalWrite(13, LED?HIGH:LOW);
+      if (current_note_ < length_) {
+	current_frequency_ = (notes_ + current_note_)->frequency;
+	current_duration_ = (notes_ + current_note_)->beats * beat_duration_;
+      }
+      else {
+	current_frequency_ = 0;
+	current_duration_ = 0;
+      }
       /*
       Serial.print("NextNote = ");
       Serial.print(current_note_);
+      Serial.print(", freq: ");
+      Serial.print(current_frequency_);
+      Serial.print(", duration: ");
+      Serial.print(current_duration_);
       Serial.print(", return ");
       Serial.println( current_note_ < length_ ? "TRUE" : "FALSE");
       */
@@ -43,41 +66,38 @@ namespace mdlib {
     }
 
     // frequency of current note.
-    int NoteFrequency() {
-      if (current_note_ < length_)
-	return notes_[current_note_].frequency;
-
-      return 0;
+    long NoteFrequency() {
+      return current_frequency_;
     }
 
     // duration of current note in milliseconds.
     int NoteDuration() {
-      if (current_note_ < length_)
-	return notes_[current_note_].beats * beat_duration_;
-
-      return 0;
+      return current_duration_;
     }
     
   private:
     int current_note_;
+    long current_frequency_;
+    int current_duration_; 
 
-    uint8_t beat_duration_; // milliseconds
+    int beat_duration_; // milliseconds
     music_note* notes_;
     int length_;
   };
 
   class Player {
   public:
-    Player() : playing_(false), tune_(0) {
+  Player() : playing_(false), tune_(0), note_start_(0), ms_(0) {
     }
     virtual ~Player(){};
 
     void SetTune(Tune* tune) { tune_ = tune; }
-
-    void Play() {
+    void SetTime(unsigned long ms) { ms_ = ms; }
+    void Play(unsigned long ms) {
+      ms_ = ms;
       if (tune_) {
 	playing_ = tune_->Start();
-	note_start_ = millis();
+	note_start_ = ms_;
 	SetFrequency(tune_->NoteFrequency());
       }
     }
@@ -88,24 +108,25 @@ namespace mdlib {
     }
 
     // Call Update() from loop() in sketch.
-    void Update() {
+    void Update(unsigned long ms) {
+      ms_ = ms;
       if (playing_) {
-        long elapsed = millis() - note_start_;
+        long elapsed = ms_ - note_start_;
 	if (elapsed >= tune_->NoteDuration()) {
 	  playing_ = tune_->NextNote();
-	  note_start_ = millis();
+	  note_start_ = ms_;
 	  SetFrequency(tune_->NoteFrequency());
 	}
       }
     }
 
   protected:
-    virtual void SetFrequency(int freq) = 0;
-
+    virtual void SetFrequency(long freq) = 0;
   private:
     bool playing_;
     Tune* tune_;
-    long note_start_;
+    unsigned long note_start_;
+    unsigned long ms_;
   };
 
 } // namespace mdlib
