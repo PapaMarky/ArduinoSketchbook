7c395da1bb39c8d3a06a8b6cd5fc7e6d43a8d522
Mark 862 <862@intrafile.com>
2014-09-03 18:16:32 -0700

    Create Shape FinishLine project

---
+++ Shape/FinishLineControlPanel/FinishLineControlPanel.ino
@@ -0,0 +1,35 @@
+/**
+ * FinishLineControlPanel for Shape Security scooter races.
+ */
+ 
+ /*
+  * State diagram: https://docs.google.com/drawings/d/1iWZqNdk4uv99Wvf8hHpwqDw-FqMTovA7hFz8g3So90I
+  */
+ 
+ 
+ // Laser sensor (photocell) - INPUT, one analog pin
+ // - laser is stand alone, battery operated device
+ 
+ // LED to indicate Laser detected - OUTPUT
+ 
+ // Serial LCD Display (Serial, needs TX/RX only uses TX)
+ 
+ // 'go' button - INPUT, one pin
+ 
+ // Christmas Tree Display (8 bit shift register)
+ 
+ // Speaker (move to DigitalClock?)
+ 
+ // Serial to Big7Segment display - INPUT/OUTPUT - TX/RX
+ 
+ // Data logger - OUTPUT (Serial, needs TX/RX, only uses TX)
+
+ // Joystick (?)
+ 
+ void setup() {
+ }
+ 
+ 
+ void loop() {
+ }
+ 
