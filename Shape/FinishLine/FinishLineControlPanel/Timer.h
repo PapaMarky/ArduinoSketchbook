d44018f43f107812a1c40342df47e1179cebfb1c
Mark 862 <862@intrafile.com>
2014-10-03 16:07:24 -0700

    FinishLine: add Timer class
    Still need to hook up firing of event.

---
+++ Shape/FinishLine/FinishLineControlPanel/Timer.h
@@ -0,0 +1,34 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+class Timer {
+  public:
+  Timer() _start(0), _active(false) {}
+  void start(unsigned long duration, int event) {
+    _start = millis();
+    _active = true;
+    _event = event;
+    _duration = duration;
+  }
+  void stop() {
+    _active = false;
+  }
+  
+  void loop() {
+    if (_active) {
+    }
+  }
+  
+  unsigned long elapsed() {
+    return _active ? millis() - _start : 0;
+  }
+  
+  private:
+  unsigned long _start;
+  unsigned long _duration;
+  boolean _active;
+  int _event;
+};
+
+#endif // TIMER_H
+
