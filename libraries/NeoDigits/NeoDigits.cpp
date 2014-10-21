4153f3567d35b6bb89624992086d1562fa86a2eb
PapaMarky <papamarky@markyshouse.com>
2014-10-21 13:33:52 -0700

    BigDigitalClock: checkpoint - got it to compile

--- libraries/NeoDigits/NeoDigits.cpp
+++ libraries/NeoDigits/NeoDigits.cpp
@@ -4,12 +4,18 @@
 #include "Adafruit_NeoPixel.h"
 #include "NeoDigits.h"
 
-NeoDigits::NeoDigits(int nDigits, int nColons, int pinNumber) : _nDigits(nDigits), _nColons(nColons) {
-  _pixels = Adafruit_NeoPixel(N_DIGITS * DIGIT_SIZE + N_COLONS * COLON_SIZE, pinNumber, NEO_GRB + NEO_KHZ800);
+const int gSegLengths[8] = {
+  SEG_A_LEN, SEG_B_LEN, SEG_C_LEN, SEG_D_LEN, SEG_E_LEN, SEG_F_LEN, SEG_G_LEN, SEG_DP_LEN
+};
+
+
+NeoDigitDisplay::NeoDigitDisplay(uint16_t nDigits, uint16_t nColons, uint8_t pinNumber)
+  : _nDigits(nDigits), _nColons(nColons),
+    _pixels(nDigits * DIGIT_SIZE + nColons * COLON_SIZE, pinNumber, (uint8_t)(NEO_GRB + NEO_KHZ800)){
 }
 
-void NeoDigits::setup() {
+void NeoDigitDisplay::setup() {
 }
 
-void NeoDigits::loop() {
+void NeoDigitDisplay::loop() {
 }
--- libraries/NeoDigits/NeoDigits.h
+++ libraries/NeoDigits/NeoDigits.h
@@ -1,22 +1,31 @@
 /**
  * NeoDigits - 7 Segment Display built from NeoPixel LEDs
  */
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-#define N_DIGITS 6
-#define N_COLONS 2
+#define N_DIGITS 1
+#define N_COLONS 0
 // Define offset of each segment from the begining of the digit
 // In my case horizontal segments (A, D, G) are 3 pixels,
 // vertical segments (B, C, E, F) are 2 and the decimal point
 // is one.
 #define SEG_A_OFF 0
-#define SEG_B_OFF SEG_A_OFF + 3
-#define SEG_C_OFF SEG_B_OFF + 2
-#define SEG_D_OFF SEG_C_OFF + 2
-#define SEG_E_OFF SEG_D_OFF + 3
-#define SEG_F_OFF SEG_E_OFF + 2
-#define SEG_G_OFF SEG_F_OFF + 2
-#define SEG_DP_OFF SEG_G_OFF + 3
-
+#define SEG_A_LEN 4
+#define SEG_B_OFF SEG_A_OFF + SEG_A_LEN
+#define SEG_B_LEN 3
+#define SEG_C_OFF SEG_B_OFF + SEG_B_LEN
+#define SEG_C_LEN 3
+#define SEG_D_OFF SEG_C_OFF + SEG_C_LEN
+#define SEG_D_LEN 4
+#define SEG_E_OFF SEG_D_OFF + SEG_D_LEN
+#define SEG_E_LEN 3
+#define SEG_F_OFF SEG_E_OFF + SEG_E_LEN
+#define SEG_F_LEN 3
+#define SEG_G_OFF SEG_F_OFF + SEG_F_LEN
+#define SEG_G_LEN 4
+#define SEG_DP_OFF SEG_G_OFF + 4
+#define SEG_DP_LEN 1
 // Number of pixels in a digit (including DP)
 #define DIGIT_SIZE SEG_DP_OFF + 1
 #define COLON_SIZE 2
@@ -25,12 +34,12 @@
 
 class NeoDigitDisplay {
  public:
-  NeoDigits(int nDigits, int nColons);
-  setup();
-  loop();
+  NeoDigitDisplay(uint16_t nDigits, uint16_t nColons, uint8_t pinNumber);
+  void setup();
+  void loop();
 
-  void SetColor(color c);
-  void SetGradiant(color top_c, color bottom_c);
+  void SetColor(uint32_t c);
+  void SetGradiant(uint32_t top_c, uint32_t bottom_c);
 
   void SetString(String str);
   void SetValue(char value[4]);
@@ -41,15 +50,16 @@ class NeoDigitDisplay {
 
   void ClearAll();
 
+  uint32_t color(uint8_t r, uint8_t g, uint8_t b) {return _pixels.Color(r, g, b);}
  private:
   
   Adafruit_NeoPixel _pixels;
 
   enum ColorScheme { SOLID, GRADIANT };
-  enum TransitionType { INSTANT, FADE }
-  int _nDigits;
-  int _nColons;
-  int _pin;
+  enum TransitionType { INSTANT, FADE };
+  uint16_t _nDigits;
+  uint16_t _nColons;
+  uint8_t _pin;
   char _current[N_DIGITS];
   char _next[N_DIGITS];
 };
